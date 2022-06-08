043590ed9688d8f3e3ed38b2e6d46deeb70ba513
OpoJack <oportojack@gmail.com>
2022-06-08 14:31:13 -0400

    Add default global page styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <div className='text-2xl text-red-500'>Hello world</div>
+      <div className='text-2xl '>Hello world</div>
     </div>
   );
 }
