4f5fb7524bf09485faf88f16220a6d5c7dc1de57
Jack <oportojack@gmail.com>
2022-06-08 15:59:48 -0400

    Adds trpc package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
-      <div className='text-2xl text-center '>Which Pokemon is uglier?</div>
+      <div className='text-2xl text-center'>Which Pokémon is Uglier?</div>
       <div className='p-2' />
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        <div className='w-16 h-16 bg-red-200'></div>
-        <div className='p-8'>vs</div>
-        <div className='w-16 h-16 bg-red-200'></div>
+        <div className='w-16 h-16 bg-red-200'>x</div>
+        <div className='p-8'>Vs</div>
+        <div className='w-16 h-16 bg-red-200'>y</div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 module.exports = {
   mode: "jit",
-  darkMode: false,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
