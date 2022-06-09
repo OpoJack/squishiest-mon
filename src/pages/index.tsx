d50fb0001f76f27ee343927ca63042301e4d2215
OpoJack <oportojack@gmail.com>
2022-06-08 21:45:50 -0400

    Set up Prisma + Planetscale settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import { useState } from "react";
+import type React from "react";
+import { inferQueryResponse } from "./api/trpc/[trpc]";
 const btn =
   "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
@@ -17,41 +19,48 @@ export default function Home() {
     updateIds(getOptionsForVote());
   };
 
-  if (firstPokemon.isLoading || secondPokemon.isLoading) return null;
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
       <div className='text-2xl text-center'>Which Pokémon is Uglier?</div>
       <div className='p-2' />
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
-        <div className='w-64 h-64 flex flex-col items-center'>
-          <img
-            alt={firstPokemon.data?.name}
-            src={firstPokemon.data?.sprites.front_default}
-            className='w-full'
-          />
-          <div className='text-xl text-center capitalize'>
-            {firstPokemon.data?.name}
-          </div>
-          <button className={btn} onClick={() => voteForUgly(first)}>
-            Uglier
-          </button>
-        </div>
-        <div className='p-8'>Vs</div>
-        <div className='w-64 h-64 flex flex-col items-center'>
-          <img
-            alt={secondPokemon.data?.name}
-            src={secondPokemon.data?.sprites.front_default}
-            className='w-full'
-          />
-          <div className='text-xl text-center capitalize'>
-            {secondPokemon.data?.name}
-          </div>
-          <button className={btn} onClick={() => voteForUgly(first)}>
-            Uglier
-          </button>
-        </div>
+        {!firstPokemon.isLoading &&
+          firstPokemon.data &&
+          !secondPokemon.isLoading &&
+          secondPokemon.data && (
+            <>
+              <PokemonListing
+                pokemon={firstPokemon.data}
+                vote={() => voteForUgly(first)}
+              />
+              <div className='p-8'>Vs</div>
+              <PokemonListing
+                pokemon={secondPokemon.data}
+                vote={() => voteForUgly(second)}
+              />
+            </>
+          )}
         <div className='p-2' />
       </div>
     </div>
   );
 }
+
+type PokemonFromServer = inferQueryResponse<"get-pokemon-by-id">;
+
+const PokemonListing: React.FC<{
+  pokemon: PokemonFromServer;
+  vote: () => void;
+}> = (props) => {
+  return (
+    <div className='flex flex-col items-center'>
+      <img src={props.pokemon.sprites.front_default} className='w-64 h-64 ' />
+      <div className='text-xl text-center capitalize -mt-8'>
+        {props.pokemon.name}
+      </div>
+      <button className={btn} onClick={() => props.vote()}>
+        Uglier
+      </button>
+    </div>
+  );
+};
