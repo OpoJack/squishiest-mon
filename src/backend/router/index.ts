2b63506b921ee41e083019347fb7fdb95762b4f7
OpoJack <oportojack@gmail.com>
2022-06-09 02:04:44 -0400

    Move data from external db to private db

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -2,17 +2,17 @@ import * as trpc from "@trpc/server";
 import { z } from "zod";
 import { prisma } from "@/backend/utils/prisma";
 
-import { PokemonClient } from "pokenode-ts";
-
 export const appRouter = trpc
   .router()
   .query("get-pokemon-by-id", {
     input: z.object({ id: z.number() }),
     async resolve({ input }) {
-      const pokemonApi = new PokemonClient();
+      const pokemon = await prisma.pokemon.findFirst({
+        where: { id: input.id },
+      });
 
-      const pokemon = await pokemonApi.getPokemonById(input.id);
-      return { name: pokemon.name, sprites: pokemon.sprites };
+      if (!pokemon) throw new Error("Doesn't exist haha");
+      return pokemon;
     },
   })
   .mutation("cast-vote", {
