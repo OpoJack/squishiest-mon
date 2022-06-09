1d899a228d6e257d628fb91cb8f18fcb2ff7f88a
Jack <oportojack@gmail.com>
2022-06-09 03:32:27 -0400

    Includes ".env" to try and fix deployment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import type React from "react";
 import { inferQueryResponse } from "./api/trpc/[trpc]";
 
 import Image from "next/image";
-
 const btn =
   "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
