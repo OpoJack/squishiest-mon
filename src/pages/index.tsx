34e3f92c9c375be4d916564bff8316cc47a2ba02
OpoJack <oportojack@gmail.com>
2022-06-08 23:32:17 -0400

    Uses Next Image loader instead of <img>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 import type React from "react";
 import { inferQueryResponse } from "./api/trpc/[trpc]";
+
+import Image from "next/image";
 const btn =
   "inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
 
@@ -16,7 +18,7 @@ export default function Home() {
 
   const voteMutation = trpc.useMutation(["cast-vote"]);
 
-  const voteForUgly = (selected: number) => {
+  const voteForSquishy = (selected: number) => {
     if (selected === first) {
       voteMutation.mutate({ votedFor: first, votedAgainst: second });
     } else {
@@ -28,7 +30,7 @@ export default function Home() {
 
   return (
     <div className='h-screen w-screen flex flex-col justify-center items-center'>
-      <div className='text-2xl text-center'>Which Pokémon is Uglier?</div>
+      <div className='text-2xl text-center'>Which Pokémon is Squishier?</div>
       <div className='p-2' />
       <div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
         {!firstPokemon.isLoading &&
@@ -38,12 +40,12 @@ export default function Home() {
             <>
               <PokemonListing
                 pokemon={firstPokemon.data}
-                vote={() => voteForUgly(first)}
+                vote={() => voteForSquishy(first)}
               />
               <div className='p-8'>Vs</div>
               <PokemonListing
                 pokemon={secondPokemon.data}
-                vote={() => voteForUgly(second)}
+                vote={() => voteForSquishy(second)}
               />
             </>
           )}
@@ -61,12 +63,17 @@ const PokemonListing: React.FC<{
 }> = (props) => {
   return (
     <div className='flex flex-col items-center'>
-      <img src={props.pokemon.sprites.front_default} className='w-64 h-64 ' />
+      <Image
+        src={props.pokemon.sprites.front_default}
+        width={256}
+        height={256}
+        layout='fixed'
+      />
       <div className='text-xl text-center capitalize -mt-8'>
         {props.pokemon.name}
       </div>
       <button className={btn} onClick={() => props.vote()}>
-        Uglier
+        Squishy
       </button>
     </div>
   );
