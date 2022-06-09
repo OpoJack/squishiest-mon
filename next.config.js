8cf8bb1050f622f8182f410d20ec46d718bc6e44
Jack <oportojack@gmail.com>
2022-06-09 03:23:23 -0400

    Attempt to fix deployment not hitting database server

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+const DATABASE_URL = process.env.DATABASE_URL;
+
 const nextConfig = {
+  future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
+      // by next.js will be dropped. Doesn't make much sense, but how it is
+      fs: false, // the solution
+    };
+
+    return config;
+  },
   reactStrictMode: true,
+  env: {
+    DATABASE_URL: DATABASE_URL,
+  },
   images: {
     domains: ["raw.githubusercontent.com"],
   },
