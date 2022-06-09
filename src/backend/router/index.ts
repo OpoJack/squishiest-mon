a5ac95ba1d06acc1889e71385d9be83b503ae820
OpoJack <oportojack@gmail.com>
2022-06-08 22:18:25 -0400

    Adds endpoint for voting in Prisma

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,17 +1,34 @@
 import * as trpc from "@trpc/server";
 import { z } from "zod";
+import { prisma } from "@/backend/utils/prisma";
 
 import { PokemonClient } from "pokenode-ts";
 
-export const appRouter = trpc.router().query("get-pokemon-by-id", {
-  input: z.object({ id: z.number() }),
-  async resolve({ input }) {
-    const api = new PokemonClient();
+export const appRouter = trpc
+  .router()
+  .query("get-pokemon-by-id", {
+    input: z.object({ id: z.number() }),
+    async resolve({ input }) {
+      const api = new PokemonClient();
 
-    const pokemon = await api.getPokemonById(input.id);
-    return { name: pokemon.name, sprites: pokemon.sprites };
-  },
-});
+      const pokemon = await api.getPokemonById(input.id);
+      return { name: pokemon.name, sprites: pokemon.sprites };
+    },
+  })
+  .mutation("cast-vote", {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteinDb = await prisma.vote.create({
+        data: {
+          ...input,
+        },
+      });
+      return { success: true, vote: voteinDb };
+    },
+  });
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
