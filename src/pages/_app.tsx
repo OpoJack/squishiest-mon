bac439a3d595b03c9a3e620fdd9dd023af97d23e
Jack <oportojack@gmail.com>
2022-06-10 10:49:13 -0400

    Attempt to fix results data not updating on deployment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,9 +11,11 @@ import type { AppRouter } from "@/backend/router";
 
 function getBaseUrl() {
   if (process.browser) return ""; // Browser should use current path
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  // if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
 
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+  // return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+
+  return `https://${process.env.VERCEL_URL}`;
 }
 
 export default withTRPC<AppRouter>({
